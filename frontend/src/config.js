94828995093b3fc4a90efcabe7482dc40015e513
mikiweys <114179857+mikiweys@users.noreply.github.com>
2024-07-05 22:40:08 -0500

    a

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,2 +1,2 @@
-export const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:5173"
-export const BACKEND_URL = process.env.BACKEND_URL || "http://localhost:8800"
+export const FRONTEND_URL = "http://localhost:5173"
+export const BACKEND_URL = "http://localhost:8800"
