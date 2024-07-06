9de5d70999defd06423420a5f03d97c437c7624d
mikiweys <114179857+mikiweys@users.noreply.github.com>
2024-07-05 22:53:24 -0500

    sin logo

--- frontend/src/pages/HomeDefault.jsx
+++ frontend/src/pages/HomeDefault.jsx
@@ -5,7 +5,6 @@ import { Link } from 'react-router-dom'
 export default function HomeDefault() {
   return (
     <div className="text-center align-items-center p-5 mt-5">
-        <img src="/logo.png" className="img-fluid" alt="" />
         <h1>Te damos la bienvenida al Chat</h1>
         <p className="text-secondary mb-4">Un chat sencillo para que puedas comunicarte con tus amigos</p>
         <Link to="/signup" className="btn btn-primary mb-5">Comenzar</Link>
